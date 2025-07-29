d0aacc137061cdbdd07cf95b0a4d0eb6791d58bf
Gourav_Tandan <140303368+Gourav990@users.noreply.github.com>
2025-07-30 00:53:53 +0530

    Update auth.js
    add line

--- src/api/auth.js
+++ src/api/auth.js
@@ -6,8 +6,8 @@ import axios from 'axios';
 // });
 
 const instance = axios.create({
-  baseURL: "/api", // 🔥 Use relative path because Vite will proxy this
-  axios.defaults.withCredentials = true;
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+  withCredentials: true,
 });
 
 
